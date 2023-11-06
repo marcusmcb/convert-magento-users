107fa4aebe80f8446f31a8b0a97b797ad2018eab
marcusmcb <marcusmcb@gmail.com>
2023-11-06 07:05:16 -0800

    updated seeder script for existing users

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ const capitalizeAddress = (address) => {
 		.join(' ')
 }
 
+function normalizeAddress(address) {
+	return address.replace(/\w\S*/g, function (txt) {
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+	})
+}
+
+function capitalizeFirstLetter(str) {
+	// console.log('NAME STRING: ', str)
+	const words = str.split(' ').filter((word) => word) // Split the string and remove empty strings
+	const capitalizedWords = words.map(
+		(word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+	)
+
+	return {
+		firstName: capitalizedWords[0] || '', // First word or empty string if no words
+		lastName: capitalizedWords.slice(1).join(' '), // Rest of the words as last name
+	}
+}
+
 const convertUserCSVToJSON = (csv) => {
 	const results = Papa.parse(csv, {
 		header: true,
@@ -32,12 +51,10 @@ const convertUserCSVToJSON = (csv) => {
 			record.zip = record.zip.padStart(5, '0')
 		}
 
-		// Normalize street address
 		if (record.street_address) {
 			record.street_address = capitalizeAddress(record.street_address)
 		}
 	})
-
 	return results.data
 }
 
@@ -47,6 +64,8 @@ let masterUserList = []
 
 const addUsersToStrapiCMS = async (customerList) => {
 	customerList.forEach((customer) => {
+		const { firstName, lastName } = capitalizeFirstLetter(customer.name)
+
 		const user = {
 			username: customer.email,
 			email: customer.email,
@@ -54,29 +73,47 @@ const addUsersToStrapiCMS = async (customerList) => {
 			confirmed: true,
 			blocked: false,
 			role: 'Public',
-			name: customer.name,
-			street_address: customer.street_address,
+			first_name: firstName,
+			last_name: lastName,
+			street_address: normalizeAddress(customer.street_address),
 			city: customer.city,
 			state: customer.state,
 			zip: customer.zip,
-		}		
+		}
+		// console.log(user)
 		masterUserList.push(user)
-	})	
+	})
 
-	// for (let user of masterUserList) {
-	// 	try {
-	// 		await axios.post('http://127.0.0.1:1337/api/auth/local/register', user, {
-	// 			headers: {
-	// 				'Content-Type': 'application/json',
-	// 				'Accept': '*/*'
-	// 			}
-	// 		})
-	// 		console.log("USER ADDED: ", user)
-	// 	} catch (error) {
-	// 		console.error("STRAPI API ERROR: ", error)
-	// 	}
-	// }
-	
+	for (let user of masterUserList) {
+		try {
+			await axios.post('http://127.0.0.1:1337/api/auth/local/register', user, {
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': '*/*'
+				}
+			})
+			console.log("USER ADDED: ", user)
+		} catch (error) {
+			console.error("STRAPI API ERROR: ", error)
+		}
+	}
 }
+setTimeout(() => {
+	console.log('---- HERE ----')
+	console.log(masterUserList[0])
+}, 1000)
 
 addUsersToStrapiCMS(customerList)
+
+// addUsersToStrapiCMS(customerList)
+
+// const getStrapiUsers = async() => {
+// 	try {
+// 		await axios.get('http://127.0.0.1:1337/api/users').then(data => console.log("DATA: ", data))
+// 	} catch (error) {
+// 		console.log("ERROR FETCHING USERS: ")
+// 		console.log(error)
+// 	}
+// }
+
+// console.log(getStrapiUsers())
